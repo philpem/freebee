2230b071b50f0be4d07656312598ef19a566b319
Philip Pemberton <philpem@philpem.me.uk>
2011-03-03 08:15:09 +0000

    add single-string logging functions to work around gcc warning

--- src/utils.h
+++ src/utils.h
@@ -6,11 +6,15 @@
 #ifndef NDEBUG
 /// Log a message to stderr
 #  define LOG(x, ...) do { fprintf(stderr, "%s:%d:%s(): " x "\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__); } while (0)
+#  define LOGS(x) do { fprintf(stderr, "%s:%d:%s(): " x "\n", __FILE__, __LINE__, __PRETTY_FUNCTION__); } while (0)
 /// Log a message to stderr if 'cond' is true
 #  define LOG_IF(cond, x, ...) do { if (cond) fprintf(stderr, "%s:%d:%s(): " x "\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__); } while (0)
+#  define LOG_IFS(cond, x) do { if (cond) fprintf(stderr, "%s:%d:%s(): " x "\n", __FILE__, __LINE__, __PRETTY_FUNCTION__); } while (0)
 #else
 #define LOG(x, ...)
+#define LOGS(x)
 #define LOG_IF(cond, x, ...)
+#define LOG_IFS(cond, x)
 #endif
 
 /// Get the number of elements in an array
