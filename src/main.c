f89f6b386146dff3c9537de0fdf123a48b342cef
shadyjesse <62037742+shadyjesse@users.noreply.github.com>
2021-03-09 13:26:01 -0500

    SDL scaling quality can now be set, 'best' works well for non-integer scaling

--- src/main.c
+++ src/main.c
@@ -400,6 +400,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error creating SDL window: %s.\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, fbc_get_string("display", "scale_quality"));  // "nearest" (default), "linear", or "best"
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_RenderSetScale(renderer, scalex, scaley);
 	if (!renderer){
